550d7045b02e45c7a038102bdabc329d933c9809
Aditya <adityajolly42@gmail.com>
2025-10-29 10:08:01 +0530

    Tailwind CSS Setup and Theming

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to Homepage!!</h1>
+      <h1 className="h1-bold">Welcome to Homepage!!</h1>
+      <h1 className="h1-bold font-inter">Welcome to Homepage!!</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Homepage!!</h1>
     </div>
   );
 };
