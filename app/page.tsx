7a61ad13398b85ae87a7cc223c20bb922c349bd2
Aditya <adityajolly42@gmail.com>
2025-10-30 00:39:44 +0530

    Implement Light & Dark mode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h1 className="h1-bold">Welcome to Homepage!!</h1>
-      <h1 className="h1-bold font-inter">Welcome to Homepage!!</h1>
-      <h1 className="h1-bold font-space-grotesk">Welcome to Homepage!!</h1>
-    </div>
-  );
+  return <div>Home Page Content</div>;
 };
 
 export default Home;
