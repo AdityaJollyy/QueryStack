1ab0e0102e68c9893e123e96f21037fe6f4019cc
Aditya <adityajolly42@gmail.com>
2025-10-31 14:48:52 +0530

    Setup Auth Layouts, Social Auth Form, Auth.js, OAuth with Google and GitHub Providers

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
-const Home = () => {
-  return <div>Home Page Content</div>;
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
+  return (
+    <>
+      <h1>Home Page</h1>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log Out</Button>
+      </form>
+    </>
+  );
 };
 
 export default Home;
